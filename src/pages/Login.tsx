525eaacae04019c697572b12bed439b53940b9f1
MACKNON VULIMU <macknonvulimu708@gmail.com>
2026-09-15 11:19:33 +0300

    ui: move forgot password below password field

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -91,12 +91,7 @@ const Login = () => {
                 </div>
               </div>
               <div className="space-y-2">
-                <div className="flex items-center justify-between">
-                  <Label htmlFor="password">Password</Label>
-                  <Link to="/forgot-password" className="text-xs text-primary hover:underline">
-                    Forgot password?
-                  </Link>
-                </div>
+                <Label htmlFor="password">Password</Label>
                 <div className="relative">
                   <Lock className="absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-muted-foreground" />
                   <Input
@@ -107,6 +102,11 @@ const Login = () => {
                     onChange={(e) => setPassword(e.target.value)}
                     className="pl-10"
                   />
+                </div>
+                <div className="text-right">
+                  <Link to="/forgot-password" className="text-xs text-primary hover:underline">
+                    Forgot password?
+                  </Link>
                 </div>
               </div>
 
